f448602138de85d29ddd860a49b354dd22e7660c
Sajid Rahman <sajidur1993@gmail.com>
2020-05-13 17:45:21 +0530

    added Cors for OPTIONS

--- app/logger.py
+++ app/logger.py
@@ -52,6 +52,7 @@ def after_request(response):
         response.headers.add('Access-Control-Allow-Origin', 'https://arena.fronteous.com')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
         return response
     now = time.time()
     duration = round(now - g.start, 2)
